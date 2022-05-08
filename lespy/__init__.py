db4b7eb7294efad7e27d21b7f84b6b13efe10a85
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2022-05-08 06:37:15 -0300

    fix: path version

--- lespy/__init__.py
+++ lespy/__init__.py
@@ -9,7 +9,7 @@ from lespy.http.request import *
 from lespy.http.response import *
 from lespy.server import run
 
-__version_info__ = (0, 1, 0)
+__version_info__ = (0, 1, 1)
 __version__ = '.'.join(map(str, __version_info__))
 __author__ = 'Natan Santos'
 __email__ = 'natansantosapps@gmail.com'
